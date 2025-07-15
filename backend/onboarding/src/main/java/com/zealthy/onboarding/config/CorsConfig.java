4765cddcafd25aadbbabf6c8ef81b60db6da7358
Chathurya Burraveni <bchathurya78@gmail.com>
2025-07-15 00:42:18 -0500

    Crosconfig

--- backend/onboarding/src/main/java/com/zealthy/onboarding/config/CorsConfig.java
+++ backend/onboarding/src/main/java/com/zealthy/onboarding/config/CorsConfig.java
@@ -15,12 +15,13 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
-                                "http://localhost:5173",
-                                "https://zealthy-onboarding-frontend.onrender.com"
+                                "http://localhost:5174",
+                                "https://zealthy-onboarding-flow-frontend.onrender.com"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(true)
+                        .maxAge(3600);
             }
         };
     }
